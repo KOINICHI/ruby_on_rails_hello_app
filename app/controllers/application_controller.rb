a871761f190cce01d35a2321ca2b0f449f13ace9
KOINICHI <koinichi.kr@gmail.com>
2016-08-07 19:33:53 -0600

    back to hihi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 	def hello
-		render html: "hihi!, 안녕"
+		render html: "hihi!"
 	end
 
 	def goodbye
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 		resources :comments
 	end
 
-	root 'application#goodbye'
+	root 'application#hello'
 end
